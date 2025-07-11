1f2a5006a03021a4e36c843aba551d86f931c576
Pedro-Porto <pportot@usp.br>
2025-07-11 12:01:13 -0300

    feat: teachers and courses schemas

--- teacherEvaluation/output/evaluation.schema.ts
+++ teacherEvaluation/output/evaluation.schema.ts
@@ -27,3 +27,27 @@ export const getActiveQuestionsOutSchema = z.object({
 });
 
 export type ActiveQuestions = z.infer<typeof getActiveQuestionsOutSchema>['questions'];
+
+
+const teacherSchema = z.object({
+    teacherId: z.coerce.number().int().positive("O ID do professor deve ser um número inteiro positivo."),
+    teacherName: z.string().min(3, "O nome do professor deve ter pelo menos 3 caracteres."),
+});
+
+export type Teacher = z.infer<typeof teacherSchema>;
+
+export const getTeachersOutSchema = z.object({
+    teachers: z.array(teacherSchema)
+});
+
+const coursesSchema = z.object({
+    courseId: z.coerce.number().int().positive("O ID do curso deve ser um número inteiro positivo."),
+    courseName: z.string().min(3, "O nome do curso deve ter pelo menos 3 caracteres."),
+    courseCode: z.string().min(3, "O código do curso deve ter pelo menos 3 caracteres."),
+});
+
+export type Course = z.infer<typeof coursesSchema>;
+
+export const getCoursesOutSchema = z.object({
+    courses: z.array(coursesSchema)
+});
