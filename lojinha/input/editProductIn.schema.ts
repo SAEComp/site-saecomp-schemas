2544c460a451d7a53107b8b46a764566a96e58a7
XandGVaz <vitorgarciavaz@gmail.com>
2025-09-04 22:14:39 -0300

    feature: variáveis de esquema de edição de produto em camelCase

--- lojinha/input/editProductIn.schema.ts
+++ lojinha/input/editProductIn.schema.ts
@@ -2,13 +2,13 @@ import { z } from "zod";
 
 // Esquema de edição produtos
 export const editProductInSchema = z.object({
-    product_id: z.coerce.number(),
+    productId: z.coerce.number(),
     name: z.string().optional(),
     value: z.coerce.number().optional(),
     description: z.string().optional(),
     quantity: z.coerce.number().optional(),
-    bar_code: z.string().length(13, {message: 'bar_code deve ter exatamente 13 caracteres'}).optional(),
-    img_url: z.string().optional(),
+    barCode: z.string().length(13, {message: 'bar_code deve ter exatamente 13 caracteres'}).optional(),
+    imgUrl: z.string().optional(),
     category: z.enum(['sweet', 'salty', 'drink']).optional(),
 });
 
