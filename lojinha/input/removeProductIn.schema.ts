09cdbf157d4aeb0f10a0d5dcb483a9110623edb1
XandGVaz <vitorgarciavaz@gmail.com>
2025-09-05 12:39:17 -0300

    fix: esquema de remoção de produto adequado para camel case

--- lojinha/input/removeProductIn.schema.ts
+++ lojinha/input/removeProductIn.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 // Esquema de remoção produtos
 export const removeProductInSchema = z.object({
-    product_id: z.coerce.number()
+    productId: z.coerce.number()
 }); 
 
 // Tipo Typescript
