f9199ce735c96d761f1f6b941b05014b313312e5
XandGVaz <vitorgarciavaz@gmail.com>
2025-09-06 15:35:36 -0300

    fix: obtenção de pedidos levando em conta nome de produtos passados como filtro

--- lojinha/input/getBuyOrderPageIn.schema.ts
+++ lojinha/input/getBuyOrderPageIn.schema.ts
@@ -5,6 +5,7 @@ export const getBuyOrderPageInSchema = z.object({
     pageSize: z.coerce.number().min(1),
     page: z.coerce.number().min(1),
     status: z.enum(['cart', 'pendingPayment', 'canceled' ,'finishedPayment']).optional(),
+    productName: z.string().or(z.array(z.string())).optional(),
     userName: z.string().optional(),
     totalValueMin: z.number().refine(val => Number.isFinite(val) && /^\d+(\.\d{1,2})?$/.test(val.toString()), {message: "O valor deve ter no máximo 2 casas decimais"}).optional(),
     totalValueMax: z.number().refine(val => Number.isFinite(val) && /^\d+(\.\d{1,2})?$/.test(val.toString()), {message: "O valor deve ter no máximo 2 casas decimais"}).optional(),
