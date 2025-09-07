e0c5e706621b90808f7943dc251d7572983c44ae
XandGVaz <vitorgarciavaz@gmail.com>
2025-09-07 20:13:24 -0300

    fix: valores totais máximos e mínimos na página de pedidos suportam passagem de string para número

--- lojinha/input/getBuyOrderPageIn.schema.ts
+++ lojinha/input/getBuyOrderPageIn.schema.ts
@@ -7,8 +7,8 @@ export const getBuyOrderPageInSchema = z.object({
     status: z.enum(['cart', 'pendingPayment', 'canceled' ,'finishedPayment']).optional(),
     productName: z.string().or(z.array(z.string())).optional(),
     userName: z.string().optional(),
-    totalValueMin: z.number().refine(val => Number.isFinite(val) && /^\d+(\.\d{1,2})?$/.test(val.toString()), {message: "O valor deve ter no máximo 2 casas decimais"}).optional(),
-    totalValueMax: z.number().refine(val => Number.isFinite(val) && /^\d+(\.\d{1,2})?$/.test(val.toString()), {message: "O valor deve ter no máximo 2 casas decimais"}).optional(),
+    totalValueMin: z.coerce.number().refine(val => Number.isFinite(val) && /^\d+(\.\d{1,2})?$/.test(val.toString()), {message: "O valor deve ter no máximo 2 casas decimais"}).optional(),
+    totalValueMax: z.coerce.number().refine(val => Number.isFinite(val) && /^\d+(\.\d{1,2})?$/.test(val.toString()), {message: "O valor deve ter no máximo 2 casas decimais"}).optional(),
     dateMin: z.coerce.date().optional(),
     dateMax: z.coerce.date().optional(),
 });
