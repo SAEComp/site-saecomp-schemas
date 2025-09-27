c1dc9f6713c058e19447f36bfe1883af2e2af509
XandGVaz <vitorgarciavaz@gmail.com>
2025-09-26 23:46:56 -0300

    fix: rotas de manipulação da chave pix funcionais

--- lojinha/input/addPixKeyIn.schema.ts
+++ lojinha/input/addPixKeyIn.schema.ts
@@ -1,10 +1,9 @@
 import z from "zod";
 
 export const addPixKeyInSchema = z.object({
-    namAccount: z.string().min(1, "O nome da conta é obrigatório"),
+    nameAccount: z.string().min(1, "O nome da conta é obrigatório"),
     pixKey: z.string().min(1, "A chave é obrigatória"),
     cityAccount: z.string().min(1, "A cidade é obrigatória"),
-    emailAccount: z.string().email("Email inválido").min(1, "O email é obrigatório"),
     tokenAccount: z.string().min(1, "O token é obrigatório"),
 });
 
--- lojinha/output/getPixKeyOut.schema.ts
+++ lojinha/output/getPixKeyOut.schema.ts
@@ -5,7 +5,6 @@ export const getPixKeyOutSchema = z.object({
     nameAccount: z.string().min(1, "O nome da conta é obrigatório"),
     pixKey: z.string().min(1, "A chave é obrigatória"),
     cityAccount: z.string().min(1, "A cidade é obrigatória"),
-    emailAccount: z.string().email("Email inválido").min(1, "O email é obrigatório"),
     tokenAccount: z.string().min(1, "O token é obrigatório"),
 }); 
 
