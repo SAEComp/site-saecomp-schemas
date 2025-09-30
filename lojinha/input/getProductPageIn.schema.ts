b6bfe13de130345fc09f2af1e618217b01810e46
XandGVaz <vitorgarciavaz@gmail.com>
2025-09-30 14:19:31 -0300

    fix: correção do mínimo de página e tamanho de página na obtenção de produtos

--- lojinha/input/getProductPageIn.schema.ts
+++ lojinha/input/getProductPageIn.schema.ts
@@ -2,8 +2,8 @@ import { z } from 'zod';
 
 // Esquema de obtenção de parâmetros de entrada para obtenção das páginas de produtos
 export const getProductPageInSchema = z.object({
-    pageSize: z.coerce.number(),
-    page: z.coerce.number(),
+    pageSize: z.coerce.number().min(1, { message: 'O tamanho da página deve ser maior ou igual a 1' }),
+    page: z.coerce.number().min(1, { message: 'A página deve ser maior ou igual a 1' }),
     category: z.enum(['sweet', 'salty', 'drink']).optional(),
     name: z.string().optional()
 });
