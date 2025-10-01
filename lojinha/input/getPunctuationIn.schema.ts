5a55dd7439b6776be6f25d3ec9f216bbdf604b8a
XandGVaz <vitorgarciavaz@gmail.com>
2025-10-01 17:28:24 -0300

    fix: correção dos esquemas de obtenção de pontuação de usuário

--- lojinha/input/getPunctuationIn.schema.ts
+++
@@ -1,7 +0,0 @@
-import {z} from "zod";
-
-export const getPunctuationInSchema = z.object({
-    userId: z.coerce.number().int().positive()
-})
-
-export type ICGetPunctuationInSchema = z.infer<typeof getPunctuationInSchema>;
