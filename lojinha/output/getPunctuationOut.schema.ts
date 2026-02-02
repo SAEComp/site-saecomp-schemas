357189777c70371eeeefec78c0401191805741a6
XandGVaz <vitorgarciavaz@gmail.com>
2026-02-02 17:32:10 -0300

    feat: ausência de pontuação do usuário não retorna erro, mas sim pontuação zero

--- lojinha/output/getPunctuationOut.schema.ts
+++ lojinha/output/getPunctuationOut.schema.ts
@@ -1,8 +1,8 @@
 import {z} from "zod";
 
 export const getPunctuationOutSchema = z.object({
-    userPunctuation: z.coerce.number().int().positive(),
-    userName: z.string().min(1).max(100)
+    userPunctuation: z.coerce.number().int().nonnegative(),
+    userName: z.string().min(0).max(100)
 })
 
 export type ICGetPunctuationOutSchema = z.infer<typeof getPunctuationOutSchema>;
